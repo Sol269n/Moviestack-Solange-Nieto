a5849077828a983bcf563cf78b0702ae5c51517c
solnieto269@gmail.com <solnieto269@gmail.com>
2024-02-23 17:54:00 -0300

    arreglos del sprint 1

--- scripts/peliculas.js
+++ scripts/peliculas.js
@@ -6,15 +6,13 @@ const div = document.createElement("div")
 div.id = "contenedor"
 div.className = "flex flex-wrap justify-center p-5 gap-3"
 
-
 main.appendChild(div)
-const llamoDiv = document.getElementById("contenedor")
 
 function crearCard(pelicula){
     return`
-    <article class="w-80 flex flex-col gap-2 p-2 text-white border rounded bg-slate-900 "> 
-        <img src="${pelicula.image}" alt="${pelicula.title}">
-        <h3 class="font-medium">${pelicula.title}</h3>
+    <article class="w-80 flex flex-col gap-2 p-2 text-white border rounded border-slate-800 bg-slate-900 "> 
+        <img class="rounded" src="${pelicula.image}" alt="${pelicula.title}">
+        <h3 class="font-medium bg-slate-800 text-center">${pelicula.title}</h3>
         <h4 class="font-semibold">${pelicula.tagline}</h4>
         <p>${pelicula.overview}</p>
     </article>
@@ -28,10 +26,5 @@ function mostrarCard(){
     }
     return cardCont
 }
-contenedor.innerHTML = mostrarCard()
+div.innerHTML = mostrarCard()
 
-function card(pelicula){
-    const article = document.createElement("article")
-    article.className = "flex flex-wrap"
-    return article
-}
