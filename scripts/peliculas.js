205ff3ec71a124757e4243ed750d4c71cf4b1e5f
solnieto269@gmail.com <solnieto269@gmail.com>
2024-02-27 08:53:41 -0300

    buscador sprint 2

--- scripts/peliculas.js
+++ scripts/peliculas.js
@@ -15,16 +15,72 @@ function crearCard(pelicula){
         <h3 class="font-medium bg-slate-800 text-center">${pelicula.title}</h3>
         <h4 class="font-semibold">${pelicula.tagline}</h4>
         <p>${pelicula.overview}</p>
+        <a href="./detalles_peliculas.html">more</a>
     </article>
     `
 }
 
-function mostrarCard(){
+function mostrarCard(listaPeliculas, elemento){
     let cardCont = ""
-    for (const pelicula of peliculas){
+    for (const pelicula of listaPeliculas){
         cardCont += crearCard(pelicula)
     }
+    if(listaPeliculas.length == 0){
+        cardCont = `<h2 class="font-semibold text-white text-2xl">no movies found</h2>`
+    }
+    
     return cardCont
 }
-div.innerHTML = mostrarCard()
+div.innerHTML = mostrarCard(peliculas, div)
 
+//[Filtros y busqueda]
+
+const inputBusqueda = document.getElementById("inputBusqueda")
+const contSelect = document.getElementById("contenedorSelect")
+
+//Obtendo generos y saco repetidos
+const genres = peliculas.map( pelicula => pelicula.genres).flat()
+
+const genresSet = new Set(genres) //Nota: permite almacenar valores unicos e iterar (por el cual saca los repetidos)
+const arrayGenres = Array.from(genresSet) // Convierto en array el set.
+
+//creo options para el select y retorno
+function crearSelect(genres){
+    return`
+    <option value="${genres}">${genres}</option>
+    `
+}
+
+const fnReduce =(template, genres) => template + crearSelect(genres)
+contSelect.innerHTML +=  arrayGenres.reduce(fnReduce, "" )
+
+//filtro peliculas por nombre
+
+inputBusqueda.addEventListener("input", () =>{
+    console.log("input buscador: ", inputBusqueda.value)
+    const moviesFiltradosG = filtrarMoviesPorGenero(peliculas, contSelect.value)
+    const moviesFiltradosN = filtroMoviesPorNombre(moviesFiltradosG, inputBusqueda.value)
+    
+    div.innerHTML = mostrarCard(moviesFiltradosN, div)
+})
+
+function filtroMoviesPorNombre(listaPeliculas, nombre){
+    return listaPeliculas.filter(pelicula => pelicula.title.toLowerCase().startsWith(nombre.toLowerCase()))
+}
+
+//escucho y filtro por genero
+contSelect.addEventListener("change",() => {
+    const moviesFiltradosG = filtrarMoviesPorGenero(peliculas, contSelect.value)
+    div.innerHTML = mostrarCard(moviesFiltradosG, div)
+
+})
+
+function filtrarMoviesPorGenero(listaPeliculas, genero){
+    if(genero.length == 0){
+        return listaPeliculas
+
+    }else{
+        return listaPeliculas.filter(pelicula => pelicula.genres.includes(genero))
+    }
+
+}
